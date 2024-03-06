422e3257fe3596945de1a343aadb2b61b0820b32
Yhuri Gross <yhurigross@gmail.com>
2024-03-06 14:53:35 -0300

    Criação de clientes e produtos mockados

---
+++ src/clientesMockados.js
@@ -0,0 +1,5 @@
+export const clientes = [
+    { nome: 'Marcos', documento: '222222222', telefone: '321-456-7440', email: 'cliente1@gmail.com', ativo: true },
+    { nome: 'Alfredo', documento: '432986046', telefone: '990-554-7543', email: 'cliente2@gmail.com', ativo: true },
+    { nome: 'Luiz', documento: '758493027', telefone: '534-645-1995', email: 'cliente3@gmail.com', ativo: false }
+  ];
---
+++ src/produtosMockados.js
@@ -0,0 +1,5 @@
+export const produtos = [
+    { nome: 'Camiseta', ativo: true },
+    { nome: 'Anel', ativo: true },
+    { nome: 'Bolsa', ativo: false }
+  ];
