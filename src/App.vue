e392f992a7a9d8a546a3234bb32e73a9e3cd724e
Yhuri Gross <yhurigross@gmail.com>
2024-03-06 15:13:34 -0300

    Componente de cadastrar cliente
    Criação do componente de cadastrar cliente

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CadastroCliente/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CadastroCliente from './components/CadastroCliente.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CadastroCliente
   }
 }
 </script>
---
+++ src/components/CadastroCliente.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2>Cadastro de Cliente</h2>
+    <form @submit.prevent="cadastrarCliente">
+      <input type="text" v-model="novoCliente.nome" placeholder="Nome">
+      <input type="text" v-model="novoCliente.documento" placeholder="Documento">
+      <input type="text" v-model="novoCliente.telefone" placeholder="Telefone">
+      <input type="email" v-model="novoCliente.email" placeholder="E-mail">
+      <button type="submit">Cadastrar</button>
+    </form>
+
+    <h3>Clientes Cadastrados</h3>
+    <ul>
+      <li v-for="cliente in clientes" :key="cliente.documento">
+        {{ cliente.nome }} - {{ cliente.documento }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { clientes } from '../clientesMockados';
+
+export default {
+  data() {
+    return {
+      clientes,
+      novoCliente: { nome: '', documento: '', telefone: '', email: '', ativo: true }
+    };
+  },
+  methods: {
+    cadastrarCliente() {
+      this.clientes.push({ ...this.novoCliente });
+      this.novoCliente = { nome: '', documento: '', telefone: '', email: '', ativo: true };
+    }
+  }
+}
+</script>
