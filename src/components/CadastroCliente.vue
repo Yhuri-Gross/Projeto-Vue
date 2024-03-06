5f39bb412329e282f9130d41039cf93e3911c402
Yhuri Gross <yhurigross@gmail.com>
2024-03-06 17:16:38 -0300

    Editar produto e cliente
    Função para editar cliente e produto

--- src/components/CadastroCliente.vue
+++ src/components/CadastroCliente.vue
@@ -1,18 +1,19 @@
 <template>
   <div>
-    <h2>Cadastro de Cliente</h2>
-    <form @submit.prevent="cadastrarCliente">
-      <input type="text" v-model="novoCliente.nome" placeholder="Nome">
-      <input type="text" v-model="novoCliente.documento" placeholder="Documento">
-      <input type="text" v-model="novoCliente.telefone" placeholder="Telefone">
-      <input type="email" v-model="novoCliente.email" placeholder="E-mail">
-      <button type="submit">Cadastrar</button>
+    <h2>{{ modoEdicao ? 'Editar Cliente' : 'Cadastro de Cliente' }}</h2>
+    <form @submit.prevent="modoEdicao ? atualizarCliente() : cadastrarCliente()">
+      <input type="text" v-model="novoCliente.nome" placeholder="Nome" required>
+      <input type="text" v-model="novoCliente.documento" placeholder="Documento" required>
+      <input type="text" v-model="novoCliente.telefone" placeholder="Telefone" required>
+      <input type="email" v-model="novoCliente.email" placeholder="E-mail" required>
+      <button type="submit">{{ modoEdicao ? 'Atualizar Cliente' : 'Cadastrar Cliente' }}</button>
     </form>
 
     <h3>Clientes Cadastrados</h3>
     <ul>
-      <li v-for="cliente in clientes" :key="cliente.documento">
+      <li v-for="(cliente, index) in clientes" :key="index">
         {{ cliente.nome }} - {{ cliente.documento }}
+        <button @click="editarCliente(index)">Editar</button>
       </li>
     </ul>
   </div>
@@ -25,13 +26,28 @@ export default {
   data() {
     return {
       clientes,
-      novoCliente: { nome: '', documento: '', telefone: '', email: '', ativo: true }
+      novoCliente: { nome: '', documento: '', telefone: '', email: '' },
+      modoEdicao: false,
+      clienteSelecionadoIndex: null
     };
   },
   methods: {
     cadastrarCliente() {
       this.clientes.push({ ...this.novoCliente });
-      this.novoCliente = { nome: '', documento: '', telefone: '', email: '', ativo: true };
+      this.novoCliente = { nome: '', documento: '', telefone: '', email: '' };
+    },
+    editarCliente(index) {
+      this.modoEdicao = true;
+      this.clienteSelecionadoIndex = index;
+      this.novoCliente = { ...this.clientes[index] };
+    },
+    atualizarCliente() {
+      if (this.clienteSelecionadoIndex !== null) {
+        this.clientes[this.clienteSelecionadoIndex] = { ...this.novoCliente };
+        this.novoCliente = { nome: '', documento: '', telefone: '', email: '' };
+        this.modoEdicao = false;
+        this.clienteSelecionadoIndex = null;
+      }
     }
   }
 }
--- src/components/CadastroProduto.vue
+++ src/components/CadastroProduto.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-      <h2>Cadastro de Produto</h2>
-      <form @submit.prevent="cadastrarProduto">
+      <h2>{{ modoEdicao ? 'Editar Produto' : 'Cadastro de Produto' }}</h2>
+      <form @submit.prevent="modoEdicao ? atualizarProduto() : cadastrarProduto()">
         <label for="nome">Nome:</label>
         <input type="text" id="nome" v-model="novoProduto.nome" required>
         <label for="ativo">Ativo:</label>
@@ -9,13 +9,14 @@
           <option value="true">Sim</option>
           <option value="false">Não</option>
         </select>
-        <button type="submit">Cadastrar</button>
+        <button type="submit">{{ modoEdicao ? 'Atualizar Produto' : 'Cadastrar Produto' }}</button>
       </form>
   
       <h3>Produtos Cadastrados</h3>
       <ul>
-        <li v-for="produto in produtos" :key="produto.nome">
+        <li v-for="(produto, index) in produtos" :key="produto.nome">
           {{ produto.nome }} - {{ produto.ativo ? 'Ativo' : 'Inativo' }}
+          <button @click="editarProduto(index)">Editar</button>
         </li>
       </ul>
     </div>
@@ -28,14 +29,30 @@
     data() {
       return {
         produtos,
-        novoProduto: { nome: '', ativo: true }
+        novoProduto: { nome: '', ativo: true },
+        modoEdicao: false,
+        produtoSelecionadoIndex: null
       };
     },
     methods: {
       cadastrarProduto() {
         this.produtos.push({ ...this.novoProduto });
         this.novoProduto = { nome: '', ativo: true };
+      },
+
+      editarProduto(index) {
+      this.modoEdicao = true;
+      this.produtoSelecionadoIndex = index;
+      this.novoProduto = { ...this.produtos[index] };
+    },
+    atualizarProduto() {
+      if (this.produtoSelecionadoIndex !== null) {
+        this.produtos[this.produtoSelecionadoIndex] = { ...this.novoProduto };
+        this.novoProduto = { nome: '', ativo: true };
+        this.modoEdicao = false;
+        this.produtoSelecionadoIndex = null;
       }
+    }
     }
   }
   </script>
